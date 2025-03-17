c175fc752317183f31db667b70698b8b03056bac
Aman Lath <85346421+Amanlath1@users.noreply.github.com>
2025-03-18 04:08:30 +0530

    Created Substring.java

---
+++ Substring.java
@@ -0,0 +1,9 @@
+public class Substring {
+    public static void main(String args[]) {
+        String text = "subscription";
+        String extractedText = text.substring(3, 9);
+        // TODO: complete the code by filling in the blanks.
+        // Note: Remove the underscore to fill up the blanks
+        System.out.println("Output: " + extractedText);
+    }
+}
