c787e0f160bcc269c77dc74aa5d3ade80dfcd2cf
Aman Lath <85346421+Amanlath1@users.noreply.github.com>
2025-03-19 05:28:27 +0530

    Create moneyCheck.java

---
+++ moneyCheck.java
@@ -0,0 +1,13 @@
+public class moneyCheck {
+     public static void main(String[] args) {
+
+         int moneyLeftWithToni =299;
+
+   //1(a): Put your if-statement here to check if moneyLeftWithToni> =300. 
+   //1(b): Put the display statements within the curly braces of the if.        
+        if(moneyLeftWithToni >= 300){
+            System.out.println("Go to Las Vegas \nEnjoy some luck games \nEnjoy some concerts \nEnjoy fine dining \nGo on a helicopter ride \nGo shop for some souvenirs");
+        }
+         System.out.println("On trip to parents.");
+     }
+ }
