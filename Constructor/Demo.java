268913b7961134653d90df0de61046f089cb98e9
primelath <85346421+primelath@users.noreply.github.com>
2025-03-31 16:58:01 +0530

    Constructor Added

---
+++ Constructor/Demo.java
@@ -0,0 +1,8 @@
+class Demo{
+    String name;
+    int age;
+    public Demo(){
+        this.name = "Aman Lath";
+        this.age = 21;
+    }
+}
---
+++ Constructor/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+        // Your code here!
+        Demo data = new Demo();
+        
+        System.out.println(data.name);
+        
+    }
+}
