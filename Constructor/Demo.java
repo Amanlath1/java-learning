adfd097d88557858d9dcdb6c42ab8f14e4b5a434
primelath <85346421+primelath@users.noreply.github.com>
2025-03-31 17:09:42 +0530

    Constructor Updated

--- Constructor/Demo.java
+++ Constructor/Demo.java
@@ -1,8 +1,16 @@
 class Demo{
     String name;
     int age;
+    
+    // Default Constructor
     public Demo(){
         this.name = "Aman Lath";
         this.age = 21;
+    }
+    
+    // Parametrized Constructor
+    public Demo(String name, int age){
+        this.name = name;
+        this.age = age;
     }
 }
--- Constructor/Main.java
+++ Constructor/Main.java
@@ -1,9 +1,11 @@
 public class Main {
     public static void main(String[] args){
         // Your code here!
-        Demo data = new Demo();
+        Demo data = new Demo(); // Default Constructor object
+        Demo data1 = new Demo("Gunika", 19); // Parametrized Constructor object
         
-        System.out.println(data.name);
+        System.out.println(data.name + " : " + data.age);
+        System.out.println(data1.name + " : " + data1.age);
         
     }
 }
